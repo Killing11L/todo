9b658b6ceec5cf7bd7c14398acd56fa6f3f3d80a
feiwei <1071005748@qq.com>
2024-05-31 17:09:25 +0800

    基础功能ok
    Signed-off-by: feiwei <1071005748@qq.com>

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -22,6 +22,15 @@ class _TodoPageState extends State<TodoPage> {
   final List<TextEditingController> _textEditControllers = [];
   int _isHoveredIndex = -1;
 
+  final ValueNotifier<bool> _taskIconNotifier = ValueNotifier<bool>(false);
+  final ValueNotifier<int> _taskIndexNotifier = ValueNotifier<int>(-1);
+
+  @override
+  void dispose() {
+    _taskIconNotifier.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     _tasks = TaskStore.getInstance().getTasks(false);
@@ -78,51 +87,129 @@ class _TodoPageState extends State<TodoPage> {
               );
             }
 
-            return Stack(
-              children: [
-                TextField(
-                  controller: _textEditControllers[index],
-                  onSubmitted: (value) {
-                    print('MMMMMM onSubmitted ${value}');
-                    TaskStore.getInstance()
-                        .editTask(uuid: _tasks[index].id, desc: value);
-                    _editIsCompleted = true;
-                    setState(() {});
-                  },
-                  onTap: () {
-                    _editIndex = index;
-                  },
-                ),
-                Positioned(
-                  right: 0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.check),
-                        onPressed: () {
-                          TaskStore.getInstance().editTask(
-                              uuid: _tasks[index].id,
-                              desc: _textEditControllers[index].text.trim(),
-                              isComplete: true);
-                          setState(() {});
-                        },
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {
-                          TaskStore.getInstance()
-                              .deleteTask(_tasks[index].id);
-                          setState(() {});
-                        },
-                      ),
-                    ],
+            return MouseRegion(
+              onEnter: (event) {
+                print('_TodoPageState onEnter');
+                _taskIconNotifier.value = true;
+                _taskIndexNotifier.value = index;
+              },
+              onExit: (event) {
+                print('_TodoPageState onExit');
+                _taskIconNotifier.value = false;
+                _taskIndexNotifier.value = -1;
+              },
+              child: Stack(
+                children: [
+                  TextField(
+                    controller: _textEditControllers[index],
+                    onSubmitted: (value) {
+                      print('MMMMMM onSubmitted ${value}');
+                      TaskStore.getInstance()
+                          .editTask(uuid: _tasks[index].id, desc: value);
+                      _editIsCompleted = true;
+                      setState(() {});
+                    },
+                    onTap: () {
+                      _editIndex = index;
+                    },
                   ),
-                ),
-              ],
+                  TaskIcons(
+                    highlightNotifier: _taskIconNotifier,
+                    index: index,
+                    indexNotifier: _taskIndexNotifier,
+                  )
+                ],
+              ),
             );
           },
         ),
+      ),
+    );
+  }
+}
+
+class TaskItem extends StatefulWidget {
+  const TaskItem({super.key});
+
+  @override
+  State<TaskItem> createState() => _TaskItemState();
+}
+
+class _TaskItemState extends State<TaskItem> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class TaskInputItem extends StatefulWidget {
+  const TaskInputItem({super.key});
+
+  @override
+  State<TaskInputItem> createState() => _TaskInputItemState();
+}
+
+class _TaskInputItemState extends State<TaskInputItem> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class TaskIcons extends StatefulWidget {
+  const TaskIcons(
+      {super.key,
+      required this.highlightNotifier,
+      required this.index,
+      required this.indexNotifier});
+  final int index;
+  final ValueNotifier<bool> highlightNotifier;
+  final ValueNotifier<int> indexNotifier;
+
+  @override
+  State<TaskIcons> createState() => _TaskIconsState();
+}
+
+class _TaskIconsState extends State<TaskIcons> {
+  @override
+  void initState() {
+    super.initState();
+    widget.highlightNotifier.addListener(_handleHighlightChange);
+  }
+
+  @override
+  void dispose() {
+    widget.highlightNotifier.removeListener(_handleHighlightChange);
+    super.dispose();
+  }
+
+  void _handleHighlightChange() {
+    setState(() {
+      // Just calling setState to rebuild the widget
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 0,
+      child: Row(
+        children: [
+          if (widget.highlightNotifier.value && (widget.indexNotifier.value == widget.index))
+            IconButton(
+              icon: const Icon(Icons.check),
+              onPressed: () {
+                setState(() {});
+              },
+            ),
+          if (widget.highlightNotifier.value && (widget.indexNotifier.value == widget.index))
+            IconButton(
+              icon: const Icon(Icons.delete),
+              onPressed: () {
+                setState(() {});
+              },
+            ),
+        ],
       ),
     );
   }
